6f660a2f53ca02e1f482344f909705fbf8d4174f
mandev8 <isalukman8@gmail.com>
2022-09-19 08:41:38 -0700

    react info site

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="my--footer">
+      <small>© 2021 mandev8 development. All rights reserved.</small>
+    </footer>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="./pub images/react-logo.png" alt="" className="nav--icon" />
+      <h3 className="nav--logo-text">React Facts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
